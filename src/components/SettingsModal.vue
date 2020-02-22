bc4ae00a406faa97cdfb0e2c3374689b96b7f283
Mitch Samuels <samuels.mitch@gmail.com>
2020-02-22 12:11:51 -0600

    Start page toggle

--- src/components/SettingsModal.vue
+++ src/components/SettingsModal.vue
@@ -9,113 +9,113 @@
 
 			<div class="modal-wrapper" v-on:click.self="closeSettings()" v-if="showModal">
 
-					<div class="modal">
+				<div class="modal">
 
-						<!-- Close Modal (optional) -->
-						<!-- <i class="far fa-times modal-close" @click="closeSettings()"></i> -->
-						<!-- Header -->
-						<div class="modal-title">Settings</div>
-						<!-- Body Content -->
-						<div class="modal-body" id="settingsBody">
+					<!-- Close Modal (optional) -->
+					<!-- <i class="far fa-times modal-close" @click="closeSettings()"></i> -->
+					<!-- Header -->
+					<div class="modal-title">Settings</div>
+					<!-- Body Content -->
+					<div class="modal-body" id="settingsBody">
 
-							<!-- General Preferences -->
-							<div class="settings-group">
+						<!-- General Preferences -->
+						<div class="settings-group">
 
-								<!-- Dark Mode -->
-								<div class="setting-toggle">
-									<div class="setting-toggle-input">
-										<input id="darkmodeToggle" type="checkbox" class="toggle" v-model="$store.getters.userPreferences.darkMode" @change="toggleDarkMode()"/>
-									</div>
-									<label class="setting-label-large" for="darkmodeToggle">
-										Dark Mode
-									</label>
+							<!-- Dark Mode -->
+							<div class="setting-toggle">
+								<div class="setting-toggle-input">
+									<input id="darkmodeToggle" type="checkbox" class="toggle" v-model="$store.getters.userPreferences.darkMode" @change="toggleDarkMode()"/>
 								</div>
-
-								<!-- UI Animations -->
-								<div class="setting-toggle">
-									<div class="setting-toggle-input">
-										<input id="animationToggle" type="checkbox" class="toggle" v-model="$store.getters.userPreferences.animations" @change="toggleAnimations()"/>
-									</div>
-									<label class="setting-label-large" for="animationToggle">
-										UI Animations
-										<small>You can turn off the UI animations like the ones that transition between pages.</small>
-									</label>
-								</div>
-
-								<!-- Start Page Selector -->
-								<label class="setting-label-large mtop-xs">
-									Start Page {{test}}
-									<small>Select the page you'd like to load first when you visit this site.</small>
+								<label class="setting-label-large" for="darkmodeToggle">
+									Dark Mode
 								</label>
-								<div class="options-display">
-									<!-- v-for over all options defined in startPages -->
-									<div v-for="option in startPages" :key="option.label">
-										<input type="radio" name="startPage" v-bind:aria-label="option.label + ' Page'" v-bind:id="option.label+'StartPage'" v-model="$store.getters.userPreferences.startPage" v-bind:value="option.path" hidden/>
-										<label class="option" v-bind:for="option.label+'StartPage'">
-											<i v-bind:class="'fas ' + option.icon"></i>
-											<span>{{option.label}}</span>
-										</label>
-									</div>
-								</div>
-
-								<!-- Clear local storage -->
-								<div class="basic-field" id="clearLocalStorage">
-									<div class="field-body">
-										<button class="button red small" type="button" aria-label="Clear Local Storage" @click="clearLocalStorage()">
-											<span>Clear Local Storage</span>
-											<i class="far fa-trash-alt"></i>
-										</button>
-										<!-- Local storage help toggle -->
-										<button id="localStorageHelpButton" @click="showLocalStorageHelp = !showLocalStorageHelp">
-											<span v-if="!showLocalStorageHelp">What's this?</span>
-											<span v-else>Hide</span>
-											<i v-bind:class="{'far fa-chevron-down': !showLocalStorageHelp, 'far fa-chevron-up': showLocalStorageHelp}"></i>
-										</button>
-									</div>
-								</div>
-
-								<!-- Paragraph explaining local storage -->
-								<transition name="basic">
-									<div id="localStorageHelp" v-if="showLocalStorageHelp">
-										Instead of accounts, this site uses your browser's local storage to remember your preferences and data. Here's what that means:
-										<ul>
-											<li>Local Storage is basically a file in your browser that this website can use to temporarily save things.</li>
-											<li>Because it's saved <b>only</b> in the browser, if you visit this site on another device or browser, you won't have your preferences or data.</li>
-											<li>Anyone who visits this site on this device/browser will have access to the data, but...</li>
-											<li>We don't save anything private or personal in there. It's mainly for settings you toggle (like remembering if you prefer dark or light mode).</li>
-										</ul>
-										If you'd like to clear the local storage and reset this site to the default settings, click the button above.
-										<br/>
-										<b>Your local storage currently looks like this:</b>
-										<!-- Code block to show existing local storage -->
-										<div class="local-storage-code-display" v-if="localStorageString">
-											<code>
-												<!-- If empty -->
-												<span v-if="localStorageString == '[]'">
-													[Your Local Storage is empty]
-												</span>
-												<span v-else>
-													{{localStorageString}}
-												</span>
-											</code>
-										</div>
-										
-									</div>
-								</transition>
-
 							</div>
 
-						</div>
-						<!-- Footer/Buttons -->
-						<div class="modal-footer center">
-							<!-- Close Modal -->
-							<button class="button invert" type="button" aria-label="Close Settings" @click="closeSettings()">
-								<span>Close Settings</span>
-								<i class="far fa-times"></i>
-							</button>
+							<!-- UI Animations -->
+							<div class="setting-toggle">
+								<div class="setting-toggle-input">
+									<input id="animationToggle" type="checkbox" class="toggle" v-model="$store.getters.userPreferences.animations" @change="toggleAnimations()"/>
+								</div>
+								<label class="setting-label-large" for="animationToggle">
+									UI Animations
+									<small>You can turn off the UI animations like the ones that transition between pages.</small>
+								</label>
+							</div>
+
+							<!-- Start Page Selector -->
+							<label class="setting-label-large mtop-xs">
+								Start Page {{test}}
+								<small>Select the page you'd like to load first when you visit this site.</small>
+							</label>
+							<div class="options-display">
+								<!-- v-for over all options defined in startPages -->
+								<div v-for="option in startPages" :key="option.label">
+									<input type="radio" name="startPage" v-bind:aria-label="option.label + ' Page'" v-bind:id="option.label+'StartPage'" v-model="$store.getters.userPreferences.startPage" v-bind:value="option.path" hidden/>
+									<label class="option" v-bind:for="option.label+'StartPage'">
+										<i v-bind:class="'fas ' + option.icon"></i>
+										<span>{{option.label}}</span>
+									</label>
+								</div>
+							</div>
+
+							<!-- Clear local storage -->
+							<div class="basic-field" id="clearLocalStorage">
+								<div class="field-body">
+									<button class="button red small" type="button" aria-label="Clear Local Storage" @click="clearLocalStorage()">
+										<span>Clear Local Storage</span>
+										<i class="far fa-trash-alt"></i>
+									</button>
+									<!-- Local storage help toggle -->
+									<button id="localStorageHelpButton" @click="showLocalStorageHelp = !showLocalStorageHelp">
+										<span v-if="!showLocalStorageHelp">What's this?</span>
+										<span v-else>Hide</span>
+										<i v-bind:class="{'far fa-chevron-down': !showLocalStorageHelp, 'far fa-chevron-up': showLocalStorageHelp}"></i>
+									</button>
+								</div>
+							</div>
+
+							<!-- Paragraph explaining local storage -->
+							<transition name="basic">
+								<div id="localStorageHelp" v-if="showLocalStorageHelp">
+									Instead of accounts, this site uses your browser's local storage to remember your preferences and data. Here's what that means:
+									<ul>
+										<li>Local Storage is basically a file in your browser that this website can use to temporarily save things.</li>
+										<li>Because it's saved <b>only</b> in the browser, if you visit this site on another device or browser, you won't have your preferences or data.</li>
+										<li>Anyone who visits this site on this device/browser will have access to the data, but...</li>
+										<li>We don't save anything private or personal in there. It's mainly for settings you toggle (like remembering if you prefer dark or light mode).</li>
+									</ul>
+									If you'd like to clear the local storage and reset this site to the default settings, click the button above.
+									<br/>
+									<b>Your local storage currently looks like this:</b>
+									<!-- Code block to show existing local storage -->
+									<div class="local-storage-code-display" v-if="localStorageString">
+										<code>
+											<!-- If empty -->
+											<span v-if="localStorageString == '[]'">
+												[Your Local Storage is empty]
+											</span>
+											<span v-else>
+												{{localStorageString}}
+											</span>
+										</code>
+									</div>
+									
+								</div>
+							</transition>
+
 						</div>
 
-					</div> <!-- End modal body -->
+					</div>
+					<!-- Footer/Buttons -->
+					<div class="modal-footer center">
+						<!-- Close Modal -->
+						<button class="button invert" type="button" aria-label="Close Settings" @click="closeSettings()">
+							<span>Close Settings</span>
+							<i class="far fa-times"></i>
+						</button>
+					</div>
+
+				</div> <!-- End modal body -->
 
 			</div>
 
@@ -142,7 +142,7 @@ export default {
 	data() {
 		return {
 			// Show settings modal, set to true on mounted.
-			showModal: true,
+			showModal: false,
 			// Toggle help
 			showLocalStorageHelp: false,
 			// Variable to hold entire *string* of local storage for displaying to user
